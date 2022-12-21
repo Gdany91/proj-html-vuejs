1fa503361e4bbaeaf8eca6a06712825a83aaf4f0
gdany91 <you@example.comdgarofalo52@gmail.com>
2022-12-21 17:27:35 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script >
+
+import AppHeader from './components/AppHeader.vue'
+import AppMain from './components/AppMain.vue'
+import AppFooter from './components/AppFooter.vue'
+
+</script>
+
+<template>
+  
+  <header>
+    <AppHeader/>
+  </header>
+
+  <main>
+    <AppMain/>
+  </main>
+
+  <footer>  
+    <AppFooter/>
+  </footer>
+
+</template>
+  
+ 
+
+<style lang="scss">
+
+ @use './styles/General.scss'  as*;
+
+</style>
